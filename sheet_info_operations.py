ee46fe7d15c01dec3c039c562dd7252e0c6ba158
Ashutosh Anand <ashutosh.anand@devslane.com>
2022-05-09 19:48:27 +0530

    Added function to get sheet_gid from sheet_name

--- sheet_info_operations.py
+++ sheet_info_operations.py
@@ -50,9 +50,31 @@ class InfoOperations:
         except Exception as exception:
             print_formatted_exception(str(exception))
 
+    @staticmethod
+    def get_sheet_gid_from_sheet_name_in_spreadsheet(sheet_name):
+        try:
+            request = sheet.get(spreadsheetId=GOOGLE_SHEET_ID)
+            result = request.execute()["sheets"]
+            sheet_obj = list(
+                filter(
+                    lambda sheet: str(sheet["properties"]["title"]) == sheet_name,
+                    result,
+                ),
+            )
+
+            if not sheet_obj:
+                print_formatted_exception("No sheet with the given sheet_name exists")
+                return
+
+            print_formatted_output(sheet_obj[0]["properties"]["sheetId"], "Sheet GID")
+
+        except Exception as exception:
+            print_formatted_exception(str(exception))
+
 
 if __name__ == "__main__":
     pass
     # InfoOperations.get_spreadsheet_info()
     # InfoOperations.get_all_sheets_name_in_spreadsheet()
     # InfoOperations.get_sheet_name_from_gid_in_spreadsheet("557067360")
+    # InfoOperations.get_sheet_gid_from_sheet_name_in_spreadsheet("First Sheet")
